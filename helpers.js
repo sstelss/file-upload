694c4afab9587f5b18094817b0cde86e1b096c27
sstelss <fantom2.00@mail.ru>
2021-02-25 22:05:45 +0300

    add createElement helper

--- helpers.js
+++ helpers.js
@@ -4,3 +4,17 @@ export function bytesToSize(bytes) {
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
 }
+
+export function createElement(tag, classList=[], content='') {
+  const element = document.createElement(tag)
+
+  if (classList.length) {
+    element.classList.add(...classList)
+  }
+
+  if (content) {
+    element.textContent = content
+  }
+
+  return element
+}
